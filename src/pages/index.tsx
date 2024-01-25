9aa91694a9b200b5d21d1358df83f3b75f35ea21
imkrmin <zum2n2@gmail.com>
2024-01-25 16:28:02 +0900

    Feat: SearchBar 컴포넌트 디자인 시스템 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
+import BackButton from "@/components/common/BackButton";
+import Header from "@/components/common/Header";
+import SearchBar from "@/components/common/SearchBar";
+
 export default function Landing() {
   return (
     <>
+      <Header />
       <div>Landing</div>
+      <BackButton />
+      <SearchBar />
     </>
   );
 }
