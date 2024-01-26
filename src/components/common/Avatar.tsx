67c478afe9acf7c4da5e33d45a9405b81c1f0a4a
buguenkwon <guen9310@gmail.com>
2024-01-26 21:00:53 +0900

    Feat: Avatars 컴포넌트 생성

--- src/components/common/Avatar.tsx
+++ src/components/common/Avatar.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 
 interface AvatarProps {
+  name?: string;
   src?: string;
 }
 
-function Avatar({ src }: AvatarProps) {
+function Avatar({ name, src }: AvatarProps) {
   const style = {
     background: src ? `url(${src})` : "orange",
     backgroundSize: "cover",
     backgroundPosition: "center",
   };
-  return <div className={`border-2 border-white rounded-full h-38 w-38`} style={style} />;
+
+  const initial = name?.charAt(0);
+  return (
+    <div
+      className={`flex items-center justify-center text-white border-2 border-white rounded-full h-38 w-38 tablet:h-34 tablet:w-34`}
+      style={style}>
+      <span>{initial}</span>
+    </div>
+  );
 }
 
 export default Avatar;
