33a2cf165901a70ada452ae5e2d8fe21a5b865a9
imkrmin <zum2n2@gmail.com>
2024-01-26 12:47:49 +0900

    Chore: index.tsx 초기화

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,6 @@
-import BackButton from "@/components/common/BackButton";
-import Header from "@/components/common/Header";
-import SearchBar from "@/components/common/SearchBar";
-
 export default function Landing() {
   return (
     <>
-      <Header />
-      <BackButton />
-      <SearchBar />
       <div>Landing</div>
     </>
   );
