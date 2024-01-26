089f1f5195bac22a484dba4704fef671a5734d99
김주현 <trust987@naver.com>
2024-01-27 02:46:06 +0900

    Feat: Dropdown 컴포넌트 기능 구현

--- src/components/common/Dropdown/Lists.tsx
+++ src/components/common/Dropdown/Lists.tsx
@@ -1,35 +1,50 @@
 import check from "../../../../public/images/check.png";
 import Image from "next/image";
-import { useState } from "react";
-import { SelectOption } from ".";
+import { ReactNode } from "react";
+import { TitleOption, MembersOption } from ".";
 
-interface ListProps {
-  items: SelectOption[];
-  selectedItem: string;
-  setSelectedItem: React.Dispatch<React.SetStateAction<string>>;
+function Option({ option, children, selectedOption, handleChange, handleProfileChange }: OptionProps) {
+  const value = "title" in option ? option.title : option.userId;
+  const handleClick = () => {
+    handleChange(value);
+    handleProfileChange && handleProfileChange(option as MembersOption);
+  };
+
+  return (
+    <li
+      className="flex gap-6 bg-white cursor-pointer options-center hover:bg-gray-EEEE rounded-4 py-5"
+      value={value}
+      onClick={handleClick}>
+      <Image
+        src={check}
+        alt="selected"
+        width={22}
+        height={22}
+        className={`${selectedOption === value ? "visible" : "invisible"}`}
+      />
+      {children}
+    </li>
+  );
 }
 
-function Lists({ items, selectedItem, setSelectedItem }: ListProps) {
+function Lists({ children }: ListsProps) {
   return (
-    <ul className="absolute left-0 flex flex-col w-full px-8 bg-white border-solid top-50 rounded-6 border-1 border-gray-D9D9 py-13 gap-13">
-      {items.map((item: SelectOption) => (
-        <li
-          key={item.title}
-          className="flex items-center gap-6 bg-white cursor-pointer hover:bg-gray-EEEE rounded-4 "
-          value={item.title}
-          onClick={() => setSelectedItem(item.title)}>
-          <Image
-            src={check}
-            alt="selected"
-            width={22}
-            height={22}
-            className={`${selectedItem === item.title ? "visible" : "invisible"}`}
-          />
-          {item.title}
-        </li>
-      ))}
+    <ul className="absolute left-0 flex flex-col w-full px-8 bg-white border-solid top-50 rounded-6 border-1 border-gray-D9D9 py-13 gap-13 z-dropdown">
+      {children}
     </ul>
   );
 }
 
-export default Lists;
+export { Option, Lists };
+
+interface ListsProps {
+  children: ReactNode;
+}
+
+interface OptionProps {
+  option: TitleOption | MembersOption;
+  children: ReactNode;
+  selectedOption: TitleOption | number;
+  handleChange: (value: string | number) => void;
+  handleProfileChange?: (option: MembersOption) => void;
+}
