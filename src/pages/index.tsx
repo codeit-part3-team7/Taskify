27d6350b5a35133a6985ac16af5cd212474758e5
9ummy9 <ria1005@kakao.com>
2024-01-27 15:29:43 +0900

    Chore: index 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import ButtonBase from "@/components/common/ButtonBase";
 
 export default function Landing() {
-  return <div>
-    <ButtonBase variant="login">로그인</ButtonBase>
-  </div>;
+  return <div>Landing</div>;
 }
