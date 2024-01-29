7c2ccdc3493eda8fa4a5df7c820dee55d0c01151
buguenkwon <guen9310@gmail.com>
2024-01-29 22:43:44 +0900

    Chore : InputField -> ModalInputFiled 로 이름 수정 및 로직 수정

--- src/components/common/ModalInputField.tsx
+++ src/components/common/ModalInputField.tsx
@@ -1,68 +1,70 @@
 import React from "react";
-import { useFormContext } from "react-hook-form";
+import { useFormContext, Controller } from "react-hook-form";
 
 interface InputFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  labelId: string;
-  name: string;
+  labelName: string;
+  labelTitle: string;
   textArea?: boolean;
+  rules?: any;
   required?: boolean;
 }
 
 interface TextAreaFieldProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
-  labelId: string;
-  name: string;
+  labelName: string;
+  labelTitle: string;
   textArea: true;
+  rules?: any;
   required?: boolean;
 }
 
 type CombinedFieldProps = InputFieldProps | TextAreaFieldProps;
 
-function ModalInputField({ labelId, name, textArea, required, ...props }: CombinedFieldProps) {
+function ModalInputField({ labelName, labelTitle, textArea, rules, required, ...props }: CombinedFieldProps) {
   const {
-    register,
     trigger,
+    control,
     formState: { errors },
   } = useFormContext();
 
-  const triggerValidationOnBlur = async () => {
-    await trigger(labelId);
-  };
-
-  const validationRules = {
-    // API 연동되면 API에서 제공하는 errorMessage 출력
-    ...(required && { required: "빈 값입니다." }),
-  };
-
-  const errorMessage = errors[labelId]?.message;
+  const errorMessage = errors[labelName]?.message;
   const errorText = typeof errorMessage === "string" ? errorMessage : "";
 
-  const fieldClass = `border-1 rounded-6 border-gray-D9D9 py-14 px-16`;
+  //blur 적용할거면 사용.
+  const triggerValidationOnBlur = async () => {
+    await trigger(labelName);
+  };
+
+  const fieldClass = `border-1 rounded-6 py-14 px-16`;
+  const errorBorder = errors[labelName] ? "border-red" : "border-gray-D9D9";
 
   return (
-    <div className="flex flex-col gap-10">
-      <label htmlFor={labelId}>
-        {name}
+    <div className="flex flex-col">
+      <label className="text-16 tablet:text-18" htmlFor={labelName}>
+        {labelTitle}
         {required && <span className="text-violet">*</span>}
       </label>
-      {textArea ? (
-        <textarea
-          {...register(labelId, validationRules)}
-          className={`${fieldClass} resize-none`}
-          id={labelId}
-          onBlur={triggerValidationOnBlur}
-          {...(props as React.TextareaHTMLAttributes<HTMLTextAreaElement>)}
-        />
-      ) : (
-        <input
-          {...register(labelId, validationRules)}
-          className={`${fieldClass} h-42 tablet:h-48`}
-          id={labelId}
-          type="text"
-          onBlur={triggerValidationOnBlur}
-          {...(props as React.InputHTMLAttributes<HTMLInputElement>)}
-        />
-      )}
-      <span className="text-red text-14">{errorText}</span>
+      <Controller
+        name={labelName}
+        control={control}
+        rules={rules}
+        defaultValue={""}
+        render={({ field }) =>
+          textArea ? (
+            <textarea
+              className={`${fieldClass} ${errorBorder} resize-none`}
+              {...field}
+              {...(props as React.TextareaHTMLAttributes<HTMLTextAreaElement>)}
+            />
+          ) : (
+            <input
+              className={`${fieldClass} ${errorBorder} h-42 tablet:h-48 `}
+              {...field}
+              {...(props as React.InputHTMLAttributes<HTMLInputElement>)}
+            />
+          )
+        }
+      />
+      {errors[labelName] && <span className="text-red">{errorText}</span>}
     </div>
   );
 }
