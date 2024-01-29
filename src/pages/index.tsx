3285919746e01894663b36ce6e345d877941841f
imkrmin <zum2n2@gmail.com>
2024-01-29 11:04:37 +0900

    Chore: index.tsx 초기화

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,3 @@
-import Comments from "@/components/modal/Comments";
-import AddImageInput from "@/components/modal/input/AddImageInput";
-import CommentInput from "@/components/modal/input/CommentInput";
-
 export default function Landing() {
-  return (
-    <>
-      <CommentInput />
-      <Comments />
-      <AddImageInput />
-      <div>Landing</div>
-    </>
-  );
+  return <div>Landing</div>;
 }
