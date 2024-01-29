b39b9ead75f14913ca08b0e96c65a62865988f82
imkrmin <zum2n2@gmail.com>
2024-01-29 11:03:25 +0900

    Fix: AddImageIput 사진 추가 이미지 오류 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,14 @@
+import Comments from "@/components/modal/Comments";
+import AddImageInput from "@/components/modal/input/AddImageInput";
+import CommentInput from "@/components/modal/input/CommentInput";
+
 export default function Landing() {
-  return <div>Landing</div>;
+  return (
+    <>
+      <CommentInput />
+      <Comments />
+      <AddImageInput />
+      <div>Landing</div>
+    </>
+  );
 }
