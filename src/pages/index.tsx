9bd6c6494d836f309dad7538c2122e7d85ce8396
9ummy9 <ria1005@kakao.com>
2024-01-29 13:48:06 +0900

    Chore: button case값 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,9 @@
+import Button from "@/components/common/Button";
+
 export default function Landing() {
-  return <div>Landing</div>;
+  return <div>
+    <Button variant="filled" width={500} height={100} fontsize={18}>로그인</Button>
+    <Button variant="ghost" width={500} height={100} fontsize={16}>거절</Button>
+    <Button variant="ghost_gray" width={500} height={100} fontsize={16}>취소</Button>
+  </div>;
 }
