628dc0471278f632c1efe04ea6d262de32b35ec2
buguenkwon <guen9310@gmail.com>
2024-01-29 22:38:43 +0900

    Chore : 모달 수정

--- src/components/modal/AlertModal.tsx
+++ src/components/modal/AlertModal.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import Modal from "../common/Modal";
 
 interface AlertModalProps {
+  text: string;
   onClose: () => void;
 }
 
-function AlertModal({ onClose }: AlertModalProps) {
+function AlertModal({ text, onClose }: AlertModalProps) {
   return (
     <Modal onClose={onClose}>
       <div className="flex justify-center items-center h-full">
-        <span>비밀번호가 일치하지 않습니다.</span>
+        <span>{text}</span>
       </div>
     </Modal>
   );
