57b1c11ba24b26564f1aa31b93851ab1e877a54e
buguenkwon <guen9310@gmail.com>
2024-01-30 21:50:34 +0900

    Chore : ProfileLabel gap 추가

--- src/components/common/ProfileLabel.tsx
+++ src/components/common/ProfileLabel.tsx
@@ -14,9 +14,14 @@ interface ProfileLabelProps {
 function ProfileLabel({ data, avatarType = "default" }: ProfileLabelProps) {
   const labelType = avatarType === "default" ? "hidden tablet:block" : "";
   const labelSize = avatarType === "modal" ? "text-12 tablet:text-14" : "text-14 tablet:text-16";
+  const labelGaps = {
+    default: "gap-12",
+    modal: "gap-8",
+    dropdown: "gap-6",
+  };
 
   return (
-    <div className="flex items-center gap-6">
+    <div className={`flex items-center ${labelGaps[avatarType]}`}>
       <Avatar nickname={data.nickname} profileImageUrl={data.profileImageUrl} avatarType={avatarType} />
       <span className={`${labelSize} text-black-3332 ${labelType}`}>{data.nickname}</span>
     </div>
