5b13ef41536ef13ade6e2d3f4211bc781c61219d
9ummy9 <ria1005@kakao.com>
2024-01-30 23:47:05 +0900

    InvitationTable 구현

--- src/components/common/InviteButton.tsx
+++ src/components/common/InviteButton.tsx
@@ -4,7 +4,7 @@ function InviteButton({ variant = "default" }) {
   const buttonStyle = (variant: string) => {
     switch (variant) {
       case "filled":
-        return "w-86 h-28 gap-6 rounded-4px text-12 bg-violet text-white tablet:w-105 tablet:h-32 tablet:text-14"; 
+        return "w-86 h-28 gap-6 rounded-4px text-12 bg-violet text-white rounded-4 tablet:w-105 tablet:h-32 tablet:text-14"; 
       case "ghost":
         return "w-73 h-40 gap-8 border rounded-8 text-14 bg-white text-gray-7874 tablet:w-109 tablet:h-36 pc:w-116 pc:h-40 pc:text-16";
       default: 
--- src/components/mydashboard/InvitationTable.tsx
+++ src/components/mydashboard/InvitationTable.tsx
@@ -10,34 +10,42 @@ function InvitationTable() {
             <div className="flex items-center gap-x-12">
                 <p className="text-12 font-normal tablet:text-14">1 페이지 중 1</p>
                 <PaginationButton />
-                <InviteButton />
+                <div className="hidden tablet:block">
+                    <InviteButton variant="filled"/>
+                </div>
             </div>
         </div>
-        <p className="text-gray-9FA6 text-14 pt-18 tablet:text-16">이메일</p>
-            {/* 아래 div안의 데이터가 1개일 때와 5번째 순서마다 border-b-1이 안보이는 기능은 기능구현 때 구현 */}
-            <div>
-                <div className="flex items-center justify-between py-12 border-b-1 border-gray-EEEE">
-                    <p className="text-14">codeitA@codeit.com</p>
-                    <Button variant="ghost" buttonType="delete">취소</Button>
-                </div>
-                <div className="flex items-center justify-between py-12 border-b-1 border-gray-EEEE">
-                    <p className="text-14">codeitB@codeit.com</p>
-                    <Button variant="ghost" buttonType="delete">취소</Button>
-                </div>
-                <div className="flex items-center justify-between py-12 border-b-1 border-gray-EEEE">
-                    <p className="text-14">codeitC@codeit.com</p>
-                    <Button variant="ghost" buttonType="delete">취소</Button>
-                </div>
-                <div className="flex items-center justify-between py-12 border-b-1 border-gray-EEEE">
-                    <p className="text-14">codeitD@codeit.com</p>
-                    <Button variant="ghost" buttonType="delete">취소</Button>
-                </div>
-                <div className="flex items-center justify-between py-12 border-b-1 border-gray-EEEE">
-                    <p className="text-14">codeitE@codeit.com</p>
-                    <Button variant="ghost" buttonType="delete">취소</Button>
-                </div>
+        <div className="pt-18 flex items-center justify-between pb-12 tablet:">
+            <p className="text-gray-9FA6 text-14 tablet:text-16">이메일</p>
+            <div className="tablet:hidden">
+                <InviteButton variant="filled"/>
             </div>
+        </div>
+            {/* 아래 div안의 데이터가 1개일 때와 5번째 순서마다 border-b-1이 안보이는 기능은 기능구현 때 구현 */}
+        <div>
+            <div className="flex items-center justify-between py-12 border-b-1 border-gray-EEEE">
+                <p className="text-14">codeitA@codeit.com</p>
+                <Button variant="ghost" buttonType="delete">취소</Button>
+            </div>
+            <div className="flex items-center justify-between py-12 border-b-1 border-gray-EEEE">
+                <p className="text-14">codeitB@codeit.com</p>
+                <Button variant="ghost" buttonType="delete">취소</Button>
+            </div>
+            <div className="flex items-center justify-between py-12 border-b-1 border-gray-EEEE">
+                <p className="text-14">codeitC@codeit.com</p>
+                <Button variant="ghost" buttonType="delete">취소</Button>
+            </div>
+            <div className="flex items-center justify-between py-12 border-b-1 border-gray-EEEE">
+                <p className="text-14">codeitD@codeit.com</p>
+                <Button variant="ghost" buttonType="delete">취소</Button>
+            </div>
+            <div className="flex items-center justify-between py-12 border-b-1 border-gray-EEEE">
+                <p className="text-14">codeitE@codeit.com</p>
+                <Button variant="ghost" buttonType="delete">취소</Button>
+            </div>
+        </div>
     </div> 
+    )
   }
   
   export default InvitationTable;
