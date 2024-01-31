93cfe92fd25dfb888d4bd28d242fbf407f6e55ec
9ummy9 <ria1005@kakao.com>
2024-01-31 13:59:20 +0900

    Chore: Button variant 추가

--- src/components/common/InviteDashTable.tsx
+++ src/components/common/InviteDashTable.tsx
@@ -1,37 +1,27 @@
 import Button from "../common/Button";
+import SearchBar from "./SearchBar";
 
 function InviteDashTable() {
     return (
         <div className="px-20 tablet:px-28">
             <div className="flex items-center justify-between pt-22 tablet:pt-26">
-                <p className="text-20 font-bold tablet:text-24">구성원</p>
-                <div className="flex items-center gap-x-12">
-                    <p className="text-12 font-normal tablet:text-14">1 페이지 중 1</p>
-                    <PaginationButton />
-                </div>
+                <p className="text-20 font-bold tablet:text-24">초대받은 대시보드</p>
             </div>
+            <div className="">
+                <SearchBar />
+            </div>
+        
             <p className="text-gray-9FA6 text-14 pt-18 tablet:text-16">이름</p>
-                {/* 아래 div안의 데이터가 1개일 때와 4번째 순서마다 border-b-1이 안보이는 기능은 기능구현 때 구현 */}
                 <div>
                     <div className="flex items-center justify-between py-12 border-b-1 border-gray-EEEE">
-
-                        <Button variant="ghost" buttonType="delete">삭제</Button>
-                    </div>
-                    <div className="flex items-center justify-between py-12 border-b-1 border-gray-EEEE">
-
-                        <Button variant="ghost" buttonType="delete">삭제</Button>
-                    </div>
-                    <div className="flex items-center justify-between py-12 border-b-1 border-gray-EEEE">
-
-                        <Button variant="ghost" buttonType="delete">삭제</Button>
-                    </div>
-                    <div className="flex items-center justify-between py-12 border-b-1 border-gray-EEEE">
-
-                        <Button variant="ghost" buttonType="delete">삭제</Button>
+                        <div className="flex gap-x-1">
+                            <Button variant="filled" buttonType="confirm">수락</Button>
+                            <Button variant="ghost" buttonType="confirm">거절</Button>
+                        </div>
                     </div>
                 </div>
         </div>
-    )
+    )  
   }
   
   export default InviteDashTable;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import InviteDashTable from "@/components/common/InviteDashTable";
 import InviteListTable from "@/components/common/InviteListTable";
 import MemberTable from "@/components/common/MemberTable";
 
 export default function Landing() {
   return <div>
-    <InviteListTable />
     <MemberTable />
+    <InviteListTable />
+    <InviteDashTable />
   </div>;
 }
