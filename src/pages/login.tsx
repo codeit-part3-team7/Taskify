0cdfec67567288b91534430607a59640ff8b48d2
김주현 <trust987@naver.com>
2024-02-02 18:41:35 +0900

    Refactor: 타입에 따라 분리한  Input Field  컴포넌트로 변경

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,11 +1,12 @@
 import AuthLayout from "@/layouts/auth";
-import { AuthInputField } from "@/components/Auth/AuthInputField";
+import EmailField from "@/components/Auth/EmailField ";
+import PasswordField from "@/components/Auth/PasswordField";
 
 export default function Login() {
   return (
     <AuthLayout type="logIn" disabled>
-      <AuthInputField labelName="이메일" type="email" id="email" placeholder="이메일을 입력해 주세요" auth />
-      <AuthInputField labelName="비밀번호" type="password" id="password" placeholder="비밀번호를 입력해 주세요" auth />
+      <EmailField />
+      <PasswordField />
     </AuthLayout>
   );
 }
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,19 +1,17 @@
 import AuthLayout from "@/layouts/auth";
 import { AuthInputField, AuthCheckBox } from "@/components/Auth/AuthInputField";
+import EmailField from "@/components/Auth/EmailField ";
+import NameField from "@/components/Auth/NameField";
+import PasswordField from "@/components/Auth/PasswordField";
+import PasswordCheckField from "@/components/Auth/PasswordCheckField";
 
 export default function SignUp() {
   return (
     <AuthLayout type="signUp" disabled>
-      <AuthInputField labelName="이메일" type="email" id="email" placeholder="이메일을 입력해 주세요" auth />
-      <AuthInputField labelName="닉네임" type="text" id="nickname" placeholder="닉네임을 입력해 주세요" auth />
-      <AuthInputField labelName="비밀번호" type="password" id="password" placeholder="비밀번호를 입력해 주세요" auth />
-      <AuthInputField
-        labelName="비밀번호 확인"
-        type="password"
-        id="password-check"
-        placeholder="비밀번호를 한번 더 입력해 주세요"
-        auth
-      />
+      <EmailField />
+      <NameField />
+      <PasswordField />
+      <PasswordCheckField />
       <AuthCheckBox />
     </AuthLayout>
   );
