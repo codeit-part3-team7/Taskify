f2fd49c4d841281af98a40ddda884cfb033b8aeb
imkrmin <zum2n2@gmail.com>
2024-02-02 14:49:52 +0900

    Feat: AddTodoButton 컴포넌트 새로운 대시보드 버전 추가

--- src/components/dashboard/AddTodoButton.tsx
+++ src/components/dashboard/AddTodoButton.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { ChipAdd } from "../common/Chips";
 
-function AddTodoButton({ onClick }: React.ButtonHTMLAttributes<HTMLButtonElement>) {
+interface AddTodoButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  title: string;
+  onClick: () => void;
+}
+
+function AddTodoButton({ title, onClick }: AddTodoButtonProps) {
+  const buttonClassName = `flex items-center gap-12 justify-center border rounded-8 h-60 tablet:h-70 w-full border-gray-D9D9 ${
+    title === "새로운 대시보드" ? "bg-white text-black grow" : ""
+  }`;
+
   return (
-    <button
-      className="flex items-center gap-12 justify-center border-1 rounded-8 h-60 tablet:h-70 w-full border-gray-D9D9"
-      onClick={onClick}>
-      <span className="text-16 tablet:text-18 font-bold">새로운 컬럼 추가하기</span>
+    <button className={buttonClassName} onClick={onClick}>
+      <span className="text-16 tablet:text-18 font-bold">{title}</span>
       <ChipAdd />
     </button>
   );
--- src/pages/mydashboard.tsx
+++ src/pages/mydashboard.tsx
@@ -1,3 +1,31 @@
+import DashboardHeader from "@/components/common/DashboardHeader";
+import SideMenu from "@/components/common/SideMenu";
+import BoardLayout from "@/layouts/board";
+import { Mock_DashBoard, Mock_MyData } from "@/components/mypage/MockData";
+import { useState } from "react";
+import InviteDashTable from "@/components/common/InviteDashTable";
+import DashboardLinkButton from "@/components/mydashboard/DashboardLinkButton";
+import AddTodoButton from "@/components/common/AddTodoButton";
+
 export default function MyDashboard() {
-  return <div>mydashboard</div>;
+  const [myData, setMyData] = useState(Mock_MyData);
+  const [dashboards, setDashboards] = useState(Mock_DashBoard);
+
+  const sideMenu = <SideMenu dashboards={dashboards} />;
+  const header = <DashboardHeader myData={myData} />;
+
+  return (
+    <BoardLayout type="myDashboard" sideMenu={sideMenu} dashboardHeader={header}>
+      <div className="flex flex-col gap-40 tablet:gap-44">
+        <div className="flex gap-8 tablet:gap-13 w-full">
+          <AddTodoButton title="새로운 대시보드" />
+          <DashboardLinkButton id={1} title="테스트" createdByMe={true} color="#7ac555" />
+          <DashboardLinkButton id={1} title="테스트" createdByMe={true} color="#7ac555" />
+        </div>
+        <div className="w-full bg-white rounded-8">
+          <InviteDashTable />
+        </div>
+      </div>
+    </BoardLayout>
+  );
 }
