6cfcf9c38a62dabd32dd293cef504eb83aa77eaf
김주현 <trust987@naver.com>
2024-02-06 05:18:31 +0900

    Feat: AddTodoButton 디자인 변경 및 모달 오픈 기능 구현

--- src/components/dashboard/AddTodoButton.tsx
+++ src/components/dashboard/AddTodoButton.tsx
@@ -1,14 +1,20 @@
-import React from "react";
-import { ChipAdd } from "../common/Chips";
+import { useToggle } from "usehooks-ts";
+import { CreateColumn } from "../modal/column";
 
-function AddTodoButton({ onClick }: React.ButtonHTMLAttributes<HTMLButtonElement>) {
+function AddTodoButton() {
+  const [columnValue, columnToggle, setColumnValue] = useToggle();
   return (
-    <button
-      className="flex items-center gap-12 justify-center border-1 rounded-8 h-60 tablet:h-70 w-full border-gray-D9D9"
-      onClick={onClick}>
-      <span className="text-16 tablet:text-18 font-bold">새로운 컬럼 추가하기</span>
-      <ChipAdd />
-    </button>
+    <>
+      <div className="fixed tablet:bottom-50 tablet:right-50 bottom-35 right-35 text-center">
+        <button
+          className="flex items-center justify-center pc:gap-4 rounded-40 pc:min-w-160 pc:h-55 w-50 h-50 bg-violet hover:bg-violet-F1EF hover:text-violet border border-gray-D9D9 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110  duration-300 text-white"
+          onClick={columnToggle}>
+          <span className="hidden pc:block font-medium">새로운 컬럼 추가</span>
+          <span className="pc:text-[25px] text-[30px] pc:translate-y-0 -translate-y-2 translate-x-[0.5px]">+</span>
+        </button>
+      </div>
+      {columnValue && <CreateColumn onClose={() => setColumnValue(false)} />}
+    </>
   );
 }
 
