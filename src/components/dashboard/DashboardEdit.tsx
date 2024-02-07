eb2d3d10754b68c6286a59cd3e0cbbe3f123f8e7
9ummy9 <ria1005@kakao.com>
2024-02-07 10:55:22 +0900

    Feat: 대시보드 이름 및 색상 수정 기능 구현

--- src/components/dashboard/DashboardEdit.tsx
+++ src/components/dashboard/DashboardEdit.tsx
@@ -1,27 +1,47 @@
+import React from "react";
 import Button from "@/components/common/Button/Button";
-import { Input } from "@/components/Auth/AuthInputField/Elements";
+import { useForm, Controller } from "react-hook-form";
 import { ChipColors } from "@/components/common/Chips";
 import { useDashboardData } from "@/hooks/useDashboardData";
+import { Input } from "@/components/Auth/AuthInputField/Elements";
+import { dashboard } from "@/lib/services/dashboards";
 
 export default function DashboardEdit() {
   const { dashboardData } = useDashboardData();
+  const { control, handleSubmit } = useForm({
+    defaultValues: {
+      title: dashboardData?.title,
+      color: dashboardData?.color,
+    },
+  });
+
+  const onSubmit = async (data) => {
+    try {
+      const response = await dashboard("put", dashboardData.id, data);
+      console.log(response); // 데이터 잘 들어갔는지 확인용
+    } catch (error) {
+      console.error("Dashboard update failed", error);
+    }
+  };
+
   return (
-    <div className="bg-white px-20 rounded-8">
+    <form className="bg-white px-20 rounded-8" onSubmit={handleSubmit(onSubmit)}>
       <div className="flex flex-col gap-y-10 pt-27 pb-20 tablet:pt-32 tablet:pb-37 tablet:flex-row tablet:justify-between tablet:items-center tablet:gap-y-0">
-        <label className="text-20 font-bold" id="username">
+        <label htmlFor="title" className="text-20 font-bold">
           {dashboardData?.title}
         </label>
-        {/* 여기 대시보드 생성 모달 참고하기 */}
-        <ChipColors selectColor={dashboardData.color} setSelectColor={() => setSelectColor(dashboardData.color)} />
+        <Controller
+          name="color"
+          control={control}
+          render={({ field }) => <ChipColors selectColor={field.value} setSelectColor={field.onChange} />}
+        />
       </div>
-      <p className="text-16 tablet:text-18 font-medium pt-10 pb-10">대시보드 이름</p>
-      {/* 여기두.. */}
-      <Input />
+      <Controller name="title" control={control} render={({ field }) => <Input {...field} id="title" />} />
       <div className="flex justify-end pt-16 tablet:pt-24 pb-20 tablet:pb-28">
-        <Button variant="filled_4" buttonType="comment">
+        <Button type="submit" variant="filled_4" buttonType="comment">
           변경
         </Button>
       </div>
-    </div>
+    </form>
   );
 }
