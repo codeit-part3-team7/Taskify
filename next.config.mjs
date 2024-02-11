1f20da191fa2c50d79b945c0a5377dc420127c9b
buguenkwon <guen9310@gmail.com>
2024-02-12 01:39:09 +0900

    Build : netlify 설정 수정

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,7 @@ const nextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
+  output: "export",
 };
 
 export default nextConfig;
